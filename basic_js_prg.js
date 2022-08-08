3704ce3a0b2ce9d7294036c408060f494fa4e9c2
Shah <Riya.Shah@wkfs-frc.local>
2022-08-08 23:02:06 +0530

    hello

---
+++ basic_js_prg.js
@@ -0,0 +1,13 @@
+var x=5;
+var y=10;
+var z=x*5;
+console.log(z);
+/Key value pair/
+var person = {
+      pname: "Riya",
+      age: 21,
+      profession: "Intern",
+    };
+    
+    console.log(person["pname"]);
+    console.log(person.age);
---
+++ basic_prg.js
@@ -0,0 +1,20 @@
+var x = 5;
+
+var y = 10;
+var z = x * y;
+console.log(z);
+
+var person = {
+  pname: "Riya",
+  age: 21,
+  profession: "Intern",
+};
+
+console.log(person["pname"]);
+console.log(person.age);
+
+if (false) {
+  console.log("this is true");
+} else {
+  console.log("false");
+}
